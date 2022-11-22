1de8a6e6ec589a1267bb01efcc45064841627d78
dhimanvizul <dhimanvizul07@gmail.com>
2022-11-22 19:51:00 +0530

    Final Commit

--- app/src/main/java/com/example/batcal/MainActivity.kt
+++ app/src/main/java/com/example/batcal/MainActivity.kt
@@ -4,12 +4,18 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.mariuszgromada.math.mxparser.Expression
 import com.example.batcal.databinding.ActivityMainBinding
+import java.text.DecimalFormat
+import kotlin.math.min
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     var isDecimalPlaced: Boolean = false
+    var isMinusPlaced: Boolean = false
+    var isOpPlaced: Boolean = false
     var result: Double = 0.0
     var saved: String = ""
     lateinit var sharedPreferences: SharedPreferences
@@ -20,110 +26,129 @@ class MainActivity : AppCompatActivity() {
         sharedPreferences = getSharedPreferences(getString(R.string.preference_fileName), Context.MODE_PRIVATE)
         setContentView(binding.root)
 
+        binding.btn0.setOnClickListener {
+            if(binding.tvInput.text.toString() != "0") {
+                binding.tvInput.text = addToInputText("0")
+                isOpPlaced = false
+            }
+        }
         binding.btn1.setOnClickListener {
-            binding.tvInput.text = binding.tvInput.text.toString() + "1"
+            binding.tvInput.text = addToInputText("1")
+            isOpPlaced = false
         }
         binding.btn2.setOnClickListener {
-            binding.tvInput.text = binding.tvInput.text.toString() + "2"
+            binding.tvInput.text = addToInputText("2")
+            isOpPlaced = false
         }
         binding.btn3.setOnClickListener {
-            binding.tvInput.text = binding.tvInput.text.toString() + "3"
+            binding.tvInput.text = addToInputText("3")
+            isOpPlaced = false
         }
         binding.btn4.setOnClickListener {
-            binding.tvInput.text = binding.tvInput.text.toString() + "4"
+            binding.tvInput.text = addToInputText("4")
+            isOpPlaced = false
         }
         binding.btn5.setOnClickListener {
-            binding.tvInput.text = binding.tvInput.text.toString() + "5"
+            binding.tvInput.text = addToInputText("5")
+            isOpPlaced = false
         }
         binding.btn6.setOnClickListener {
-            binding.tvInput.text = binding.tvInput.text.toString() + "6"
+            binding.tvInput.text = addToInputText("6")
+            isOpPlaced = false
         }
         binding.btn7.setOnClickListener {
-            binding.tvInput.text = binding.tvInput.text.toString() + "7"
+            binding.tvInput.text = addToInputText("7")
+            isOpPlaced = false
         }
         binding.btn8.setOnClickListener {
-            binding.tvInput.text = binding.tvInput.text.toString() + "8"
+            binding.tvInput.text = addToInputText("8")
+            isOpPlaced = false
         }
         binding.btn9.setOnClickListener {
-            binding.tvInput.text = binding.tvInput.text.toString() + "9"
-        }
-        binding.btn0.setOnClickListener {
-            binding.tvInput.text = binding.tvInput.text.toString() + "0"
+            binding.tvInput.text = addToInputText("9")
+            isOpPlaced = false
         }
         binding.btnDot.setOnClickListener {
             if (!isDecimalPlaced) {
-                binding.tvInput.text = binding.tvInput.text.toString() + "."
+                binding.tvInput.text = addToInputText(".")
                 isDecimalPlaced = true
             }
         }
+
         binding.apply {
             btnAdd.setOnClickListener {
                 if(tvInput.text != "") {
                     saved += tvInput.text.toString()+"+"
                 }
-                if(tvInput.text != "") {
-                    result += tvInput.text.toString().toDouble()
-                    tvOutput.text = result.toString()
-                    tvInput.text = ""
+                if(tvInput.text != "" && !isOpPlaced) {
+                    tvInput.text = addToInputText("+")
+                    isOpPlaced = true
+                }
+                if(tvInput.text != "" && tvInput.text.last() != '+' && isOpPlaced) {
+                    tvInput.text = tvInput.text.subSequence(0,tvInput.text.length-1)
+                    tvInput.text = addToInputText("+")
                 }
             }
             btnSub.setOnClickListener {
                 if(tvInput.text != "") {
                     saved += tvInput.text.toString()+"-"
                 }
-                if (tvInput.text != "") {
-                    if (result == 0.0) {
-                        result = tvInput.text.toString().toDouble() - result
-                    } else result -= tvInput.text.toString().toDouble()
-                    tvOutput.text = result.toString()
-                    tvInput.text = ""
+                if (tvInput.text != "" && !isOpPlaced) {
+                    tvInput.text = addToInputText("-")
+                    isOpPlaced = true
+                }
+                if(tvInput.text != "" && tvInput.text.last() != '-' && isOpPlaced) {
+                    tvInput.text = tvInput.text.subSequence(0,tvInput.text.length-1)
+                    tvInput.text = addToInputText("-")
                 }
             }
             btnMul.setOnClickListener {
                 if(tvInput.text != "") {
                     saved += tvInput.text.toString()+"×"
                 }
-                if(tvInput.text != "") {
-                    if(result == 0.0) {
-                        result = tvInput.text.toString().toDouble()
-                    }
-                    else result *= tvInput.text.toString().toDouble()
-                    tvOutput.text = result.toString()
-                    tvInput.text = ""
+                if(tvInput.text != "" && !isOpPlaced) {
+                    tvInput.text = addToInputText("×")
+                    isOpPlaced = true
+                }
+                if(tvInput.text != "" && tvInput.text.last() != '×' && isOpPlaced) {
+                    tvInput.text = tvInput.text.subSequence(0,tvInput.text.length-1)
+                    tvInput.text = addToInputText("×")
                 }
             }
             btndiv.setOnClickListener {
                 if(tvInput.text != "") {
                     saved += tvInput.text.toString()+"/"
                 }
-                if(tvInput.text != "") {
-                    if(result == 0.0) {
-                        result = tvInput.text.toString().toDouble()
-                    }
-                    else {
-                        if(tvInput.text == "0" || tvInput.text == "-0.0"||tvInput.text == "0.0" || tvInput.text == "-0") {
-                            tvOutput.text = "ERROR"
-                            tvInput.text = ""
-                            return@setOnClickListener
-                        }
-                        else result /= tvInput.text.toString().toDouble()
-                    }
-                    tvOutput.text = result.toString()
-                    tvInput.text = ""
+                if(tvInput.text != "" && !isOpPlaced) {
+                    tvInput.text = addToInputText("÷")
+                    isOpPlaced = true
+                }
+                if(tvInput.text != "" && tvInput.text.last() != '÷' && isOpPlaced) {
+                    tvInput.text = tvInput.text.subSequence(0,tvInput.text.length-1)
+                    tvInput.text = addToInputText("÷")
                 }
             }
             btnPlusMinus.setOnClickListener {
-                if (tvInput.text == "") tvInput.text = "-"
-                else if (tvInput.text.first() != '-') {
-                    tvInput.text = (tvInput.text.toString().toDouble()*(-1)).toString()
+                if(tvInput.text == "") {
+                    tvInput.text = addToInputText("-")
+                    isMinusPlaced = true
                 }
-                else tvInput.text = (tvInput.text.toString().toDouble()*(-1)).toString()
+
+                if (tvInput.text.last() != '-' && !isMinusPlaced) {
+                    tvInput.text = addToInputText("-")
+                    isMinusPlaced = true
+                }
+                if(tvInput.text.last() == '-' && isMinusPlaced) {
+                    tvInput.text.replace(Regex("-"),"")
+                    isMinusPlaced = false
+                }
             }
             btnAllClear.setOnClickListener {
                 tvInput.text = ""
                 tvOutput.text = ""
                 result = 0.0
                 isDecimalPlaced = false
+                isOpPlaced = false
                 if(saved != "") savePreference(saved)
                 saved = ""
             }
@@ -131,12 +156,39 @@ class MainActivity : AppCompatActivity() {
                 if (tvInput.text != "") {
                     if (tvInput.text.last() == '.') isDecimalPlaced = false
                     tvInput.text = tvInput.text.toString().subSequence(0, tvInput.text.length - 1)
+                    isOpPlaced = false
                 }
             }
             imgEqual.setOnClickListener {
-                tvOutput.text = ""
-                tvOutput.text = sharedPreferences.getString("input","0.0")
+                showResult()
             }
+        }
+    }
+    private fun addToInputText(buttonValue: String): String {
+        return binding.tvInput.text.toString() + buttonValue
+    }
+    private fun getInputExpression(): String {
+        var expression = binding.tvInput.text.replace(Regex("÷"), "/")
+        expression = expression.replace(Regex("×"), "*")
+        return expression
+    }
+
+    private fun showResult() {
+        try {
+            val expression = getInputExpression()
+            if(expression.contains("÷0")) {
+                binding.tvOutput.text = "ERROR"
+                return
+            }
+            val result = Expression(expression).calculate()
+            if (result.isNaN()) {
+                binding.tvOutput.text = "ERROR"
+            } else {
+                binding.tvOutput.text = DecimalFormat("0.######").format(result).toString()
+            }
+        } catch (e: Exception) {
+
+            binding.tvOutput.text = "ERROR"
         }
     }
     private fun savePreference(str: String) {
